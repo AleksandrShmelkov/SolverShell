f2eb133f55863dcfd6b69619bc19d9f967ce2398
AleksandrShmelkov <acc.solver.shell@gmail.com>
2022-12-20 09:06:27 +0300

    Change sshell.cpp

--- src/sshell.cpp
+++ src/sshell.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     SOSH_Functions func;
-    SOSH_DeclareFunction("summ", SOSH_Functions &func);
-    std::cout << "Get: " << func::get(0) << std::endl;
+    SOSH_DeclareFunction("summ", SOSH_Functions& func);
+    std::cout << "Get: " << func.get(0) << std::endl;
     return 0;
 };
