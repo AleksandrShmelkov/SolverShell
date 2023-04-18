5f4ad0dbf6464a6d34aa1ded7525bac471991f79
AleksandrShmelkov <118812542+AleksandrShmelkov@users.noreply.github.com>
2023-04-18 20:21:19 +0300

    edit sosh_function

--- src/API/SOSH_Function.h
+++ src/API/SOSH_Function.h
@@ -51,31 +51,53 @@ protected:
     std::optional<std::type_index> r_type;
 public:
     virtual ~SOSH_Function2_Base() = default;
-    template<typename R, typename... Args>
+
+    template<class R, class... Args>
     static SOSH_Function2_Base* Create(std::string name, R (*func)(Args...)){
         return new SOSH_Function2<R, Args...>(name, func);
     };
+
     std::string GetName(){
         return name;
     };
+
     std::string GetType(){
         return type.value().name();
     };
+
     std::string GetReturnType(){
         return r_type.value().name();
     };
+
     template<typename R, typename... Args>
     auto apply(Args&&... args){
         std::tuple<Args...> arg_tuple(std::forward<Args>(args)...);
-        std::string args_type_controller = typeid(arg_tuple).name();
-        R result;
-        std::string return_type_controller = typeid(&result).name();
-        //std::cout << "args2: " << args_type_controller << std::endl;
-        //std::string result = applyWrapper(return_type_controller, args_type_controller, (sizeof...(args))+1, std::forward<Args>(args)..., &result);
-        std::string info = applyWrapper(return_type_controller, args_type_controller, 2, arg_tuple, &result);
-        return result;
+        std::tuple<Args&...> ref_tuple = std::apply([](auto&... args){ return std::make_tuple(std::ref(args)...); }, arg_tuple);
+
+        std::vector<std::string> args_type_controller;
+        args_type_controller.reserve(sizeof...(Args));
+        std::apply([&args_type_controller](auto&&... elems){
+            (args_type_controller.emplace_back(typeid(elems).name()), ...);
+        }, arg_tuple);
+        //std::vector<bool> ref_vector = {std::is_reference<Args>::value...};
+        std::string return_type_controller = typeid(R*).name();
+        if constexpr (std::is_void_v<R>) {
+            std::string info = applyWrapper(return_type_controller, args_type_controller, sizeof...(args), ref_tuple);
+        } else {
+            R result;
+            std::string info = applyWrapper(return_type_controller, args_type_controller, sizeof...(args), ref_tuple, &result);
+            return result;
+        };
     };
-    virtual std::string applyWrapper(std::string &return_type_controller, std::string &args_type_controller, int count, ...) = 0;
+
+    template<typename R, typename... Args>
+    auto apply2(Args&&... args){
+        auto func = dynamic_cast<SOSH_Function2<R, Args...> *>(this);
+        if( func == nullptr ) { throw std::runtime_error("casting failed"); };
+        return func->apply(std::forward<Args>(args)...);
+    };
+
+    virtual std::string applyWrapper(std::string &return_type_controller, std::vector<std::string> &args_base_type_controller, int count, ...) = 0;
 };
 
 template<class R, class... Args>
@@ -83,59 +105,74 @@ class SOSH_Function2 : public SOSH_Function2_Base {
 private:
     typedef R (*Func)(Args...);
     Func func;
-    std::tuple<ToType<Args>...> args;
-    std::vector<SOSH_Token> tokens;
+    std::tuple<Args...> args;
+    //SOSH_Token token_return;
+    std::vector<SOSH_Token> tokens_args;
 public:
-    SOSH_Function2(std::string s, Func f) : func(f), args(std::tuple<ToType<Args>...>{}) {
+    SOSH_Function2(std::string s, Func f) : func(f), args(std::tuple<Args...>{}) {
         this->name = s;
         this->type = typeid(R(Args...));
         this->r_type = typeid(R);
     };
-    SOSH_Function2(const SOSH_Function2& other) : func(std::move(other.func)), args(other.args) { // copy constructor 
+
+    SOSH_Function2(const SOSH_Function2& other) : func(other.func), args(other.args) { // copy constructor 
         this->name = other.name;
         this->type = other.type;
         this->r_type = other.r_type;
     };
+
     SOSH_Function2(SOSH_Function2&& other) noexcept : func(std::move(other.func)), args(std::move(other.args)) { // move constructor  
         this->name = std::move(other.name);
         this->type = std::move(other.type);
         this->r_type = std::move(other.r_type);
     };
-    void AddToken(SOSH_Token token){
-        tokens.push_back(token);
+
+    void DeclareReturn(SOSH_Token token){
+        //token_return = token;
     };
+
+    void DeclareArgs(SOSH_Token token){
+        tokens_args.push_back(token);
+    };
+
     R apply(Args&&... args){
         return func(std::forward<Args>(args)...);
     };
+
     R apply(std::tuple<Args...> args_tuple){
         return std::apply(func, std::move(args_tuple));
     };
-    std::string applyWrapper(std::string &return_type_controller, std::string &args_type_controller, int count, ...) override {
-        //std::cout << "return_type_controller: " << return_type_controller << std::endl;
-        //std::cout << "R*: " << typeid(R*).name() << std::endl;
-        //std::cout << "args_type_controller: " << args_type_controller << std::endl;
-        //std::cout << "args: " << typeid(args).name() << std::endl;
-        //std::cout << "args: " << (args_type_controller != typeid(args).name()) << std::endl;
+
+    std::string applyWrapper(std::string &return_type_controller, std::vector<std::string> &args_base_type_controller, int count, ...) override {
+        std::vector<std::string> args_type_controller;
+        args_type_controller.reserve(sizeof...(Args));
+        std::apply([&args_type_controller](auto&&... elems){
+            (args_type_controller.emplace_back(typeid(elems).name()), ...);
+        }, args);
+        
+        if (return_type_controller != typeid(R*).name()) { return "Несоответствие возвращаемого типа."; };
+        if (args_base_type_controller.size() != args_type_controller.size()) { return "Несоответствие количества аргументов."; };
+        for (std::size_t i = 0; i < args_base_type_controller.size(); ++i) {
+            if (args_base_type_controller[i] != args_type_controller[i]) {
+                return "Несоответствие аргумента №"+std::to_string(i)+".";
+            };
+        };
+        
         va_list ptr_type_controller;
         va_start(ptr_type_controller, count);
-        //std::tuple<ToType<Args>...> args_tuple(std::forward<Args>(va_arg(ptr_type_controller, Args))...);
-        if ((args_type_controller != typeid(args).name()) || (return_type_controller != typeid(R*).name())){
-            return "Несоответствие типов.";
-        };
-        std::tuple<ToType<Args>...> args_tuple = va_arg(ptr_type_controller, std::tuple<ToType<Args>...>);
-        //std::cout << "args_tuple: " << typeid(args_tuple).name() << std::endl;
-        auto result = va_arg(ptr_type_controller, R*);
-        va_end(ptr_type_controller);
-
-        //auto args_tuple_reverse = tuple_reverse(args_tuple);
+        std::tuple<Args&...> args_tuple = va_arg(ptr_type_controller, std::tuple<Args&...>);
+        //using conditional_tuple = std::tuple<std::conditional_t<std::is_rvalue_reference_v<Args>, Args&, Args>...>;
+        //conditional_tuple args_tuple = va_arg(ptr_type_controller, conditional_tuple);
         if constexpr (std::is_void_v<R>) {
             apply(args_tuple);
-            return "0";
+            return "ok void";
         } else {
             auto res = apply(args_tuple);
+            auto result = va_arg(ptr_type_controller, R*);
             *result = res;
-            return "0";
+            return "ok return";
         };
+        va_end(ptr_type_controller);
 
         //std::tuple<int, double, std::string> tp2(1, 2.0, std::string("c"));
         //std::tuple<float, int, std::string> tpl_template(0.0f, 0, std::string(""));
@@ -145,6 +182,159 @@ public:
         //auto tpl_reversed = tuple_reverse(tpl);
     };
 };
+
+
+
+
+
+
+
+
+
+/*
+template <class data_type>
+class copy_on_write
+{
+public:
+    copy_on_write(data_type* data)
+        : m_data(data) {
+    }
+    data_type const* operator -> () const {
+        return m_data.get();
+    }
+    data_type* operator -> () {
+        if (!m_data.unique())
+            m_data.reset(new data_type(*m_data));
+        return m_data.get();
+    }
+private:
+    std::shared_ptr<data_type> m_data;
+};
+
+class object {
+public:
+    object() = default;
+    template <class T>
+    void set(T data){
+        m_data = data;
+    };
+    auto get(){
+        return m_data;
+    };
+protected:
+    class data;
+private:
+    copy_on_write<data> m_data;
+};*/
+
+
+/*
+class SOSH_Function3_Base {
+protected:
+    std::string name;
+    std::optional<std::type_index> r_type;
+public:
+    virtual ~SOSH_Function3_Base() = default;
+
+    template<typename R>
+    static SOSH_Function3_Base* Create(std::string name, R* func){
+        return new SOSH_Function3<R>(name, func);
+    };
+
+    std::string GetName(){
+        return name;
+    };
+
+    std::string GetReturnType(){
+        return r_type.value().name();
+    };
+
+    template<typename R, typename... Args>
+    auto apply(Args&&... args){
+        typedef R (*typereturn)(Args...) ;
+        auto func = dynamic_cast<SOSH_Function3<R>*>(this);
+
+        //std::tuple<Args...> test(std::forward<Args>(args)...);
+        //std::cout << "SOSH_Function3_Base test: " << typeid(test).name() << std::endl;
+        //std::cout << "SOSH_Function3_Base func: " << typeid(func).name() << std::endl;
+
+        //if( func == nullptr ) { throw std::runtime_error("casting failed"); };
+        
+        return func->apply(std::forward<Args>(args)...);
+        //return 0;
+        //return static_cast<SOSH_Function3<R>&>(*this).apply_impl(std::forward<Args>(args)...);
+    };
+};
+
+template<typename R>
+class SOSH_Function3;
+
+template<typename R, typename... Args>
+class SOSH_Function3<R(Args...)> : public SOSH_Function3_Base {
+private:
+    typedef R(*Func)(Args...);
+    Func func;
+public:
+    SOSH_Function3(std::string s, Func f) : func(f) {
+        this->name = s;
+        this->r_type = typeid(R);
+    };
+    
+    SOSH_Function3(const SOSH_Function3& other) : func(other.func) { // copy constructor 
+        this->name = other.name;
+        this->r_type = other.r_type;
+    };
+
+    SOSH_Function3(SOSH_Function3&& other) noexcept : func(std::move(other.func)) { // move constructor  
+        this->name = std::move(other.name);
+        this->r_type = std::move(other.r_type);
+    };
+
+    auto apply(Args... args) {
+        std::cout << "Vhod" << std::endl;
+        std::tuple<Args...> test(args...);
+        std::cout << "SOSH_Function3 test: " << typeid(test).name() << std::endl;
+        std::cout << "SOSH_Function3 func: " << typeid(func).name() << std::endl;
+        return func(args...);
+    };
+};
+*/
+
+
+//  ------------------------------------------------
+
+/*
+template<typename R>
+class SOSH_Function3 : public SOSH_Function3_Base {
+private:
+    typedef R* Func;
+    Func func;
+public:
+    SOSH_Function3(std::string s, Func f) : func(f) {
+        this->name = s;
+        this->r_type = typeid(R);
+    };
+    
+    SOSH_Function3(const SOSH_Function3& other) : func(other.func) { // copy constructor 
+        this->name = other.name;
+        this->r_type = other.r_type;
+    };
+
+    SOSH_Function3(SOSH_Function3&& other) noexcept : func(std::move(other.func)) { // move constructor  
+        this->name = std::move(other.name);
+        this->r_type = std::move(other.r_type);
+    };
+
+    template<typename... Args>
+    auto apply(Args&&... args) {
+        std::cout << "Vhod" << std::endl;
+        std::tuple<Args...> test(std::forward<Args>(args)...);
+        std::cout << "SOSH_Function3 test: " << typeid(test).name() << std::endl;
+        std::cout << "SOSH_Function3 func: " << typeid(func).name() << std::endl;
+        //return func(std::forward<Args>(args)...);
+        return 0;
+    };
+};*/
 
 class SOSH_Function {
 private:
--- src/sshell.cpp
+++ src/sshell.cpp
@@ -1,6 +1,7 @@
 ﻿#include "API/sshell_api.h"
 
 int sum(int x, double y, std::string z) {
+    std::cout << typeid(z).name() << std::endl;
     std::cout << "x = " << x << " y = " << y << " z = " << z << "\n";
     return x + y;
 };
@@ -9,18 +10,62 @@ void print() {
     std::cout << "Message" << std::endl;
 };
 
+int add(int a, int b) {
+    std::cout << "a = " << a << " b = " << b << "\n";
+    return a + b;
+}
+
 void test_sum_int_double(){
     SOSH_Function2 SOSH_Function2_sum("sum", sum);
     double result = SOSH_Function2_sum.apply(4,5,"QWERTY");
     std::cout << "Result: " << result << std::endl << std::endl;
 
     SOSH_Function2_Base& SOSH_Function2_Base_sum = SOSH_Function2_sum;
-    double result2 = SOSH_Function2_Base_sum.apply<int>(4,5.0,std::string("QWERTY"));
-    std::cout << "Result: " << result2 << std::endl;
+    //SOSH_Function2_Base* SOSH_Function2_Base_sum = &SOSH_Function2_sum;
+
+    int a = 4;
+    double b = 5.0;
+    std::string c = "QWERTY";
+
+    int result2 = SOSH_Function2_Base_sum.apply<int>(4,5.0,std::string("QWERTY"));
+    std::cout << "Result: " << result2 << std::endl << std::endl;
+
+    int result3 = SOSH_Function2_Base_sum.apply<int>(a,b,c);
+    std::cout << "Result: " << result3 << std::endl;
+};
+
+void test_print(){
+    SOSH_Function2 SOSH_Function2_print("print", print);
+    SOSH_Function2_print.apply();
+
+    SOSH_Function2_Base& SOSH_Function2_Base_print = SOSH_Function2_print;
+    SOSH_Function2_Base_print.apply<void>();
+};
+
+void test_add(){
+    SOSH_Function2 SOSH_Function2_add("add", add);
+    int result = SOSH_Function2_add.apply(1,2);
+    std::cout << "Result: " << result << std::endl << std::endl;
+
+    SOSH_Function2_Base& SOSH_Function2_Base_add = SOSH_Function2_add;
+    int a = 1;
+    int b = 2;
+    int result2 = SOSH_Function2_Base_add.apply<int>(1,2);
+    std::cout << "Result: " << result2 << std::endl << std::endl;
+    int result3 = SOSH_Function2_Base_add.apply<int>(a,b);
+    std::cout << "Result: " << result3 << std::endl;
 };
 
 void test_SOSH_Function2_Base_Create(){
-    auto SOSH_Function2_sum = SOSH_Function2_Base::Create("sum", sum);
+    auto SOSH_Function2_add = SOSH_Function2_Base::Create("add", add);
+    int result = SOSH_Function2_add->apply2<int>(1,2);
+    std::cout << "SOSH_Function2_add: " << result << std::endl << std::endl;
+    delete SOSH_Function2_add;
+
+    auto func = SOSH_Function2_Base::Create("add", add);
+    int result2 = func->apply<int>(1,2);
+    std::cout << "func: " << result2 << std::endl;
+    delete func;
 };
 
 void test_AddToken(){
@@ -29,16 +74,47 @@ void test_AddToken(){
     SOSH_Token token2(Token_t::SOSH_INT, "4");
     SOSH_Token token3(Token_t::SOSH_DOUBLE, "5.0");
     SOSH_Token token4(Token_t::SOSH_STRING, "QWERTY");
-    SOSH_Function2_sum.AddToken(token1);
-    SOSH_Function2_sum.AddToken(token2);
-    SOSH_Function2_sum.AddToken(token3);
-    SOSH_Function2_sum.AddToken(token4);
+    SOSH_Function2_sum.DeclareReturn(token1);
+    SOSH_Function2_sum.DeclareArgs(token2);
+    SOSH_Function2_sum.DeclareArgs(token3);
+    SOSH_Function2_sum.DeclareArgs(token4);
 };
+/*
+void test_sosh3(){
+    SOSH_Function3 SOSH_Function3_sum("sum", sum);
+    int result = SOSH_Function3_sum.apply(4,5,"QWERTY");
+    std::cout << "Result: " << result << std::endl << std::endl;
+
+    //SOSH_Function3_Base& SOSH_Function3_Base_sum = SOSH_Function3_sum;
+    //SOSH_Function3_Base* SOSH_Function3_Base_sum = &SOSH_Function3_sum;
+    std::shared_ptr<SOSH_Function3_Base> SOSH_Function3_Base_sum = std::make_shared<decltype(SOSH_Function3_sum)>(SOSH_Function3_sum);
+
+    auto result2 = SOSH_Function3_Base_sum->apply<int>(4,5.0,std::string("QWERTY"));
+    std::cout << "Result: " << typeid(result2).name() << std::endl << std::endl;
+};
+*/
 
 int main() {
-    system("chcp 65001");
+    //system("chcp 65001");
 
+    std::cout << "\n ***  test_sum_int_double  ***" << std::endl << std::endl;
     test_sum_int_double();
+
+    std::cout << "\n ***  test_print  ***" << std::endl << std::endl;
+    test_print();
+
+    std::cout << "\n ***  test_add  ***" << std::endl << std::endl;
+    test_add();
+
+    std::cout << "\n ***  test_SOSH_Function2_Base_Create  ***" << std::endl << std::endl;
     test_SOSH_Function2_Base_Create();
+
+    std::cout << "\n ***  test_AddToken  ***" << std::endl << std::endl;
     test_AddToken();
+
+    //std::cout << "\n ***  test_sosh3  ***" << std::endl << std::endl;
+    //test_sosh3();
+
+    std::cout << std::endl;
+
 };
